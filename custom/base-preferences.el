2d27273f54417617ec6dc9f9762c853a66c68675
Ryan Durling <ryan.p.durling@gmail.com>
2020-04-04 14:50:37 -0500

    updated

--- custom/base-preferences.el
+++ custom/base-preferences.el
@@ -191,3 +191,17 @@
       (goto-char (+ origin (* (length region) arg) arg)))))
 
 (global-set-key (kbd "C-c d") 'duplicate-current-line-or-region)
+
+(use-package multiple-cursors
+  :ensure t
+  :config
+
+  ;; When you have an active region that spans multiple lines,
+  ;; the following will add a cursor to each line:
+  (global-set-key (kbd "C-c m c") 'mc/edit-lines)
+
+  ;; When you want to add multiple cursors not based on
+  ;; continuous lines, but based on keywords in the buffer, use:
+  (global-set-key (kbd "C-c m n") 'mc/mark-next-like-this)
+  (global-set-key (kbd "C-c m b") 'mc/mark-previous-like-this)
+  (global-set-key (kbd "C-c m a") 'mc/mark-all-like-this))

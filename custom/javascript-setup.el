f9739416b58413aa9f11d91dd378a2fe135609ad
Ryan Durling <ryan.p.durling@gmail.com>
2020-04-04 16:55:22 -0500

    tern again

--- custom/javascript-setup.el
+++ custom/javascript-setup.el
@@ -1,3 +1,3 @@
-(add-to-list 'load-path "/Users/ryandurling/.emacs.d/custom-downloads/tern/emacs")
+(add-to-list 'load-path "/Users/ryandurling/.emacs.d/submodules/tern/emacs")
 (autoload 'tern-mode "tern.el" nil t)
 (add-hook 'js-mode-hook (lambda () (tern-mode t)))

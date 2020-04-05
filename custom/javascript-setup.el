a130bd2f881d714d6bba21d5783153ec0367544a
Ryan Durling <ryan.p.durling@gmail.com>
2020-04-04 22:31:39 -0500

    some stuff

--- custom/javascript-setup.el
+++ custom/javascript-setup.el
@@ -1,3 +1,74 @@
 (add-to-list 'load-path "/Users/ryandurling/.emacs.d/submodules/tern/emacs")
 (autoload 'tern-mode "tern.el" nil t)
 (add-hook 'js-mode-hook (lambda () (tern-mode t)))
+
+(use-package add-node-modules-path
+  :ensure t
+  :config (eval-after-load 'js-mode
+	    '(add-hook 'js-mode-hook #'add-node-modules-path)))
+
+(use-package auto-complete
+  :ensure t
+  :config
+  (add-hook 'js-mode-hook (lambda () (auto-complete-mode t)))
+  (eval-after-load 'tern
+    '(progn
+       (require 'tern-auto-complete)
+       (tern-ac-setup))))
+
+(use-package exec-path-from-shell
+  :ensure t
+  :custom
+  (exec-path-from-shell-check-startup-files nil)
+  :config
+  (push "HISTFILE" exec-path-from-shell-variables)
+  (exec-path-from-shell-initialize))
+
+(add-hook 'after-init-hook 'global-company-mode)
+
+(use-package indium
+  :ensure t
+  :config
+  (add-hook 'js-mode-hook #'indium-interaction-mode))
+
+(use-package flycheck
+  :defer 1
+  :ensure t
+  :init (global-flycheck-mode)
+  :config
+  (setq-default flycheck-disabled-checkers
+		(append flycheck-disabled-checkers
+			'(javascript-jshint)))
+
+  (flycheck-add-mode 'javascript-eslint 'js-mode)
+
+  (setq-default flycheck-disabled-checkers
+		(append flycheck-disabled-checkers
+			'(json-jsonlist)))
+
+  (defun my/use-eslint-from-node-modules ()
+    (let* ((root (locate-dominating-file
+		  (or (buffer-file-name) default-directory)
+		  "node_modules"))
+	   (eslint (and root
+			(expand-file-name "node_modules/eslint/bin/eslint.js"
+					  root))))
+      (when (and eslint (file-executable-p eslint))
+	(setq-local flycheck-javascript-eslint-executable eslint))))
+  (add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules))
+
+(setq js-indent-level 2)
+
+(defun eslint-fix-file ()
+  (interactive)
+  (message "eslint --fixing the file" (buffer-file-name))
+  (shell-command (concat "node_modules/eslint/bin/eslint.js --fix " (buffer-file-name))))
+
+(defun eslint-fix-file-and-revert ()
+  (interactive)
+  (eslint-fix-file)
+  (revert-buffer t t))
+
+(add-hook 'js-mode-hook
+	  (lambda ()
+	    (add-hook 'after-save-hook #'eslint-fix-file-and-revert)))
